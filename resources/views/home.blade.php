f72a4ddf806143eb3b356b98cdab71ab2a2d3981
Brian Marandof <brianmasta23@gmail.com>
2022-11-30 11:18:30 +0900

    title edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>KBS | Mimikaa</title>
+    <title>KBS | Mimika</title>
 
     <!-- Favicons -->
   <link href="assets/img/kbs.png" rel="icon">
